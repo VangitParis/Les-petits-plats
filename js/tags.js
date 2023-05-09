271573cc7d1bd12fa35427084ab4eecd7c67cb51
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-05-09 11:55:30 +0200

    modif tags.js
    ajout du filtre par tag

--- js/tags.js
+++ js/tags.js
@@ -1,34 +1,34 @@
+import { applyFilterByTags } from "./index.js";
 export class Tags {
-  constructor(tagLinks, recipes,uniqueIngredients) {
+  constructor(tagLinks, recipes) {
     this.tagLinks = tagLinks;
     this.recipes = recipes;
-    this.uniqueIngredients = uniqueIngredients;
     this.sectionTag = document.getElementById("section-tag");
-    this.displayTags();
-    
+    this.selectedTags = [];
+    this.addTags();
+    this.addTagsWithEnterKey();
   }
 
-  displayTags() {
-    // Tableau pour stocker les Tags
-    const selectedTags =[]
-
-    // Ajouter un écouteur d'événement pour chaque lien de tag
+  addTags() {
     this.tagLinks.forEach((tagLink) => {
       tagLink.addEventListener("click", (e) => {
         e.preventDefault();
-        
+
         const currentTag = tagLink.innerText;
 
         // Vérifier si le tag existe déjà dans le DOM
         const tagIsCreate = document.getElementById("tag-id-" + currentTag);
-
         if (tagIsCreate) {
+          // Supprimer le tag du tableau des tags sélectionnés
+          const index = this.selectedTags.indexOf(currentTag);
+          if (index > -1) {
+            this.selectedTags.splice(index, 1);
+          }
+
           // Supprimer le lien du tableau des ingredients, appareils, ustensiles
           const recipeList = Array.from(
             document.getElementsByClassName("list-group-item")
           );
-
-          // Trouver l'élément de la liste correspondant à l'élément de tag
           let matchingLink = null;
           recipeList.forEach((link) => {
             if (link.innerText === currentTag) {
@@ -42,83 +42,69 @@ export class Tags {
             const index = recipeList.indexOf(matchingLink);
             recipeList.splice(index, 1);
           }
-          
+
+          // Appliquer le filtre
+          applyFilterByTags();
           return;
         }
 
         // Le tag n'existe pas encore, alors on le créé
+        
         const tag = document.createElement("ul");
         tag.classList.add("btn", "btn-sm", "d-flex", "mt-n1", "tag");
-        tag.id = `tag-id-${currentTag}`;
+        tag.id = `tag-id-${ currentTag }`;
 
+        tagLink.dataset.tag = currentTag;
         // Ajouter une classe au tag en fonction de son type
         if (tagLink.classList.contains("tag-ingredient")) {
           tag.classList.add("tag-ingredient");
-          
         } else if (tagLink.classList.contains("tag-appliance")) {
           tag.classList.add("tag-appliance");
-          
         } else if (tagLink.classList.contains("tag-ustensil")) {
           tag.classList.add("tag-ustensil");
-          
         }
 
-        const spanTag = document.createElement("li");
-        spanTag.textContent = currentTag;
-
+        const tagElement = document.createElement("li");
+        tagElement.textContent = currentTag;
+        tagElement.classList.add("selected");
         const iconCloseTag = document.createElement("img");
         iconCloseTag.setAttribute("src", "../assets/iconCloseTag.svg");
         iconCloseTag.classList.add("icon-close-tag");
 
         this.sectionTag.appendChild(tag);
-        tag.appendChild(spanTag);
+        tag.appendChild(tagElement);
         tag.appendChild(iconCloseTag);
 
         iconCloseTag.addEventListener("click", () => {
           tag.remove();
+          tagElement.classList.remove("selected");
           // Réinitialiser le tag s'il est supprimé
-          const index = selectedTags.indexOf(currentTag);
+          const index = this.selectedTags.indexOf(currentTag);
           if (index > -1) {
-            selectedTags.splice(index, 1);
+            this.selectedTags.splice(index, 1);
           }
 
-          // Ajouter le tag au tableau des tags sélectionnés
-          selectedTags.push(currentTag);
+          // Appliquer le filtre
+          applyFilterByTags();
         });
-       
+
+        // Ajouter le tag au tableau des tags sélectionnés
+        this.selectedTags.push(currentTag);
+
+        // Appliquer le filtre
+        applyFilterByTags();
       });
     });
-
   }
-  filterTags() {
-    const filteredRecipes = [];
   
-   
-  
-    for (let i = 0; i < this.recipes.length; i++) {
-      const recipe = this.recipes[i];
-      let recipeMatchesAllTags = true;
-  
-      // Vérifier que la recette contient tous les tags sélectionnés
-      this.tagLinks.forEach(tag => {
-        const tagIngredientMatch = recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(tag));
-        const tagApplianceMatch = recipe.appliance.toLowerCase().includes(tag);
-        const tagUstensilMatch = recipe.ustensils.some(ustensil => ustensil.toLowerCase().includes(tag));
-  
-        if (!(tagIngredientMatch || tagApplianceMatch || tagUstensilMatch)) {
-          recipeMatchesAllTags = false;
-        }
-      });
-  
-      if (recipeMatchesAllTags) {
-        filteredRecipes.push(recipe);
-      }
-      
-    }
-  
-    return filteredRecipes;
+  addTagsWithEnterKey() {
+    this.tagLinks.forEach((tagLink) => {
+     
+      tagLink.addEventListener("Enter", this.addTags);
+     
+    })
   }
-}
   
+ 
+  }
   
-
