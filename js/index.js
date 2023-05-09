8bc04dd5a47aa18ad6f51ed5490d3bf1a4f1f171
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-05-09 11:54:42 +0200

    ajout applyfilter dans index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,60 @@ advancedSearchInputs.forEach((advancedSearch) => {
   advancedSearch.addEventListener("input", searchHandler);
 });
 
+export function applyFilterByTags() {
+  // Récupérer les tags sélectionnés
+  const selectedTags = Array.from(
+    document.getElementsByClassName("selected")
+  ).map((tag) => tag.textContent.toLowerCase());
 
+  // Filtrer les recettes qui correspondent aux tags sélectionnés
+  const filteredRecipes = recipes.filter((recipe) => {
+    // Vérifier si tous les tags sélectionnés sont présents dans les ingrédients, appareils et ustensiles de la recette
+    let recipeTagFound = [
+      ...recipe.ingredients.map((ingredient) =>
+        ingredient.ingredient.toLowerCase()
+      ),
+      recipe.appliance.toLowerCase(),
+      ...recipe.ustensils.map((ustensil) => ustensil.toLowerCase()),
+    ];
+
+    const allSelectedTagsFound = selectedTags.every((tag) =>
+      recipeTagFound.includes(tag)
+    );
+    if (!allSelectedTagsFound) return false;
+
+    // Vérifier si au moins un tag sélectionné est présent dans la recette
+    selectedTags.some((tag) => {
+      // Vérifier si le tag est présent dans les ingrédients, les appareils ou les ustensiles de la recette
+      recipe.ingredients.forEach((ingredient) => {
+        if (ingredient.ingredient.toLowerCase().includes(tag)) {
+          recipeTagFound = true;
+        }
+      });
+      if (recipe.appliance.toLowerCase().includes(tag)) {
+        recipeTagFound = true;
+      }
+      recipe.ustensils.forEach((ustensil) => {
+        if (ustensil.toLowerCase().includes(tag)) {
+          recipeTagFound = true;
+        }
+      });
+      return recipeTagFound;
+    });
+
+    return recipeTagFound ? recipe : null;
+  });
+
+  //Afficher les recettes filtrées
+  const section = document.getElementById("cards");
+  section.innerHTML = "";
+
+  if (filteredRecipes.length === 0) {
+    displayRecipes(recipes);
+  } else {
+    displayRecipes(filteredRecipes); // actualise l'interface
+  }
+}
 
 // Afficher toutes les recettes initialement
 new Dropdown(recipes);
