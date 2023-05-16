9773579c6bf61502fb8d879744b99fe0d271c08c
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-05-16 18:20:57 +0200

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,53 @@
 import { recipes } from "./mock/recipes.js";
-import { Dropdown } from "./Dropdowns.js";
+import { FilterRecipesWithLoop } from "./algorithm/algorithmSearchBar.js";
+import { Dropdown } from "./Classes/Dropdowns.js";
 import { removeDiacritics } from "./utils.js";
-import { displayRecipes } from "./recipesCards.js";
+import { displayRecipes } from "./template/recipeCards.js";
 
 
+
+let filterUniqueRecipes = [];
+// Fonction qui gère la recherche et le filtre des recettes
+function handleSearch() {
+  const searchInput = document.getElementById("searchInput");
+  const searchText = searchInput.value.trim().toLowerCase();
+  const filterInSearchBar = new FilterRecipesWithLoop(recipes, searchText);
+  const filteredRecipesByText = filterInSearchBar.filterRecipesByText(
+    recipes,
+    searchText
+  );
+  const filteredRecipesByKeyword = filterInSearchBar.filterRecipesByKeywords(
+    recipes,
+    searchText
+  );
+
+  // Fusionner les résultats des deux filtres en une seule liste de recettes uniques
+  filterUniqueRecipes = [...new Set([...filteredRecipesByText, ...filteredRecipesByKeyword])];
+
+  const section = document.getElementById("cards");
+  section.innerHTML = "";
+
+  if (filterUniqueRecipes.length === 0) {
+    const divMessage = document.createElement("div");
+    const message = document.createElement("p");
+    message.textContent =
+      'Aucune recette ne correspond à votre critère... vous pouvez chercher "tarte aux pommes", "poisson", etc';
+    message.classList.add("error");
+    divMessage.appendChild(message);
+    section.appendChild(divMessage);
+  } else {
+    new Dropdown(filterUniqueRecipes);
+    
+    displayRecipes(filterUniqueRecipes); // actualise l'interface
+  }
+}
+
+// Ajouter un événement de saisie sur la barre de recherche
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchBtn");
+searchInput.addEventListener("input", handleSearch);
+searchInput.addEventListener("Enter", handleSearch);
+searchButton.addEventListener("click", handleSearch);
 
 //Ajouter un évènement de saisie sur la la recherche avancée
 const searchIngredients = document.getElementById("inputSearchIngredients");
@@ -17,16 +61,17 @@ const advancedSearchInputs = [
 
 // Filtre les recettes et met à jour les élément dans les dropdowns
 function advancedSearch(text) {
-   // Vérifier si l'élément existe déjà dans la liste
-   const existingTag = document.getElementById(`tag-id-${text}`);
+  // Vérifier si l'élément existe déjà dans la liste
+  const existingTag = document.getElementById(`tag-id-${text}`);
 
-   if (existingTag) {
-     return;
-   }
+  if (existingTag) {
+    return;
+  }
 
-  const dropdown = new Dropdown(recipes);
+  const dropdown = new Dropdown(filterUniqueRecipes);
   dropdown.specifiesSearch();
   const filteredRecipes = dropdown.filterRecipes();
+  
   //Afficher les recettes filtrées
   const section = document.getElementById("cards");
   section.innerHTML = "";
@@ -39,15 +84,15 @@ advancedSearchInputs.forEach((advancedSearchInput) => {
   advancedSearchInput.addEventListener("Enter", advancedSearch);
 });
 
-export function applyFilterByTags(text) {
-  
+export function applyFilterByTags() {
+  console.log(filterUniqueRecipes);
   // Récupérer les tags sélectionnés
   const selectedTags = Array.from(
     document.getElementsByClassName("selected")
   ).map((tag) => removeDiacritics(tag.textContent).toLowerCase());
 
   // Filtrer les recettes qui correspondent aux tags sélectionnés
-  const filteredRecipes = recipes.filter((recipe) => {
+  const filteredRecipes = filterUniqueRecipes.filter((recipe) => {
     // Vérifier si tous les tags sélectionnés sont présents dans les ingrédients, appareils et ustensiles de la recette
     const ingredientsRecipes = recipe.ingredients.map((ingredient) =>
       removeDiacritics(ingredient.ingredient).toLowerCase()
@@ -94,7 +139,7 @@ export function applyFilterByTags(text) {
   const section = document.getElementById("cards");
   section.innerHTML = "";
 
-  if (filteredRecipes.length === 0) {
+  if (filterUniqueRecipes.length === 0) {
     const divMessage = document.createElement("div");
     const message = document.createElement("p");
     message.textContent =
@@ -108,6 +153,8 @@ export function applyFilterByTags(text) {
   }
 }
 
+
 // Afficher toutes les recettes initialement
 new Dropdown(recipes);
+
 displayRecipes(recipes);
