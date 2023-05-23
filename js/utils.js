c3a525649a059f2adc94bb500eac6eb761336d1b
VangitParis <102918421+VangitParis@users.noreply.github.com>
2023-05-23 12:39:25 +0200

    chenge utils

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,3 @@
-import { Dropdown } from "./Dropdowns.js";
-import { recipes } from "./mock/recipes.js";
-
 export const removeDiacritics = (element) => {
   if (typeof element !== "string") {
     throw new TypeError("il faut une string");
